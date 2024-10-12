48ee869a87bcbd2d7e37989922bf9f3f6eea6320
hapontez <harold.v.aponte.z@gmail.com>
2024-10-11 21:46:00 -0400

    Correccion Commit con Patricia

--- SANITARIO_DW/Schema/Tables/dbo/FacEstudio.sql
+++ SANITARIO_DW/Schema/Tables/dbo/FacEstudio.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[FacEstudio] (
     [Id_FacEstudio] INT IDENTITY(1,1) NOT NULL,
-	[Id_Paciente] INT NOT NULL,
-	[Id_Hospital] INT NOT NULL,
-	[Id_Medico] INT NOT NULL,
+	[SK_Paciente] INT NOT NULL,
+	[SK_Hospital] INT NOT NULL,
+	[SK_Medico] INT NOT NULL,
 	[ID_Fecha] INT NOT NULL,
-   	[Precio] NVARCHAR(150) NOT NULL,
+   	[Precio] FLOAT NOT NULL,
 	[ID_Fecha_Estudio] DATETIME NOT NULL,
     [ID_Fecha_Entrega] DATETIME NOT NULL,
-    --CONSTRAINT [PK_Estudio_FacEstudio] PRIMARY KEY CLUSTERED ([Id_FacEstudio] ASC),
+    CONSTRAINT [PK_Estudio_FacEstudio] PRIMARY KEY CLUSTERED ([Id_FacEstudio] ASC),
+	CONSTRAINT [FK_Estudio_Paciente] FOREIGN KEY ([SK_Paciente]) REFERENCES [dbo].[DimPaciente] ([SK_Paciente]),
+	CONSTRAINT [FK_Estudio_Hospital] FOREIGN KEY ([SK_Hospital]) REFERENCES [dbo].[DimHospital] ([SK_Hospital]),
+	CONSTRAINT [FK_Estudio_Medico] FOREIGN KEY ([SK_Medico]) REFERENCES [dbo].[DimMedico] ([SK_Medico])
 );
--- SANITARIO_DW/Schema/Tables/dbo/FacVisita.sql
+++ SANITARIO_DW/Schema/Tables/dbo/FacVisita.sql
@@ -1,19 +1,18 @@
 ﻿CREATE TABLE [dbo].[FacVisita] (
     [Id_FacVisita] INT IDENTITY(1,1) NOT NULL,
-    [Id_Paciente] INT NOT NULL,
-    [Id_Hospital] INT NOT NULL,
-    [Id_Medico] INT NOT NULL,
+    [SK_Paciente] INT NOT NULL,
+    [SK_Hospital] INT NOT NULL,
+    [SK_Medico] INT NOT NULL,
     [Id_Visita] INT NOT NULL,
     [Id_Fecha] INT NOT NULL,
     [Precio_total] FLOAT NULL,
 	[es_Reconsulta] int NULL,
     [Id_Visita_inicial] INT NOT NULL,
-	[Diagnostico] FLOAT NULL,
-	[datos_adicionales] FLOAT NULL,
-    [RowVersion] INT NOT NULL,
+	[Diagnostico] NVARCHAR(100) NULL,
+	[datos_adicionales] NVARCHAR(200) NULL,    
     CONSTRAINT [PK_Visita_FacVisita] PRIMARY KEY CLUSTERED ([Id_FacVisita] ASC),
-    --CONSTRAINT [FK_Visita_Paciente] FOREIGN KEY ([Id_Paciente]) REFERENCES [dbo].[DimPaciente] ([Id_Paciente]),
-	--CONSTRAINT [FK_Visita_Hospital] FOREIGN KEY ([Id_Hospital]) REFERENCES [dbo].[DimHospital] ([Id_DimHospital]),
-	--CONSTRAINT [FK_Visita_Medico] FOREIGN KEY ([Id_Medico]) REFERENCES [dbo].[DimMedico] ([Id_Medico]),
-	--CONSTRAINT [FK_Visita_Tiempo] FOREIGN KEY ([Id_Fecha]) REFERENCES [dbo].[DimTiempo] ([Id_Tiempo])
+    CONSTRAINT [FK_Visita_Paciente] FOREIGN KEY ([SK_Paciente]) REFERENCES [dbo].[DimPaciente] ([SK_Paciente]),
+	CONSTRAINT [FK_Visita_Hospital] FOREIGN KEY ([SK_Hospital]) REFERENCES [dbo].[DimHospital] ([SK_Hospital]),
+	CONSTRAINT [FK_Visita_Medico] FOREIGN KEY ([SK_Medico]) REFERENCES [dbo].[DimMedico] ([SK_Medico]),
+	CONSTRAINT [FK_Visita_Tiempo] FOREIGN KEY ([Id_Fecha]) REFERENCES [dbo].[DimTiempo] ([Id_Tiempo])
 );
