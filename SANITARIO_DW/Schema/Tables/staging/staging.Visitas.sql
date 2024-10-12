26c6eb5093bfd0dfaa8b1275c57a752b485f7520
ipaguilar <ipaguilar@gmail.com>
2024-10-12 01:22:28 -0400

    Àgrega staging visita

---
+++ SANITARIO_DW/Schema/Tables/staging/staging.Visitas.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[FS_visita] INT NOT NULL PRIMARY KEY, 
+    [FK_Pacitente] INT NULL, 
+    [FK_hospital] INT NULL, 
+    [FK_Medico] INT NULL, 
+    [Diagnóstico] NUMERIC NULL, 
+    [Detalle en horarios.] NCHAR(10) NULL, 
+    [Fecha_ingreso] DATE NULL, 
+    [Fecha_Salida] DATE NULL, 
+    [Id_cama] NCHAR(10) NULL
+)
