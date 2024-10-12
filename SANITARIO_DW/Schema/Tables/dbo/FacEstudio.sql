b8814b9e29ae2d1ea2c4eccf31208a4e34046293
ipaguilar <ipaguilar@gmail.com>
2024-10-11 22:36:35 -0400

    Corrección llaves foraneas de tiempo

--- SANITARIO_DW/Schema/Tables/dbo/FacEstudio.sql
+++ SANITARIO_DW/Schema/Tables/dbo/FacEstudio.sql
@@ -5,10 +5,13 @@
 	[SK_Medico] INT NOT NULL,
 	[ID_Fecha] INT NOT NULL,
    	[Precio] FLOAT NOT NULL,
-	[ID_Fecha_Estudio] DATETIME NOT NULL,
-    [ID_Fecha_Entrega] DATETIME NOT NULL,
+	[ID_Fecha_Estudio] INT NOT NULL,
+    [ID_Fecha_Entrega] INT NOT NULL,
     CONSTRAINT [PK_Estudio_FacEstudio] PRIMARY KEY CLUSTERED ([Id_FacEstudio] ASC),
 	CONSTRAINT [FK_Estudio_Paciente] FOREIGN KEY ([SK_Paciente]) REFERENCES [dbo].[DimPaciente] ([SK_Paciente]),
 	CONSTRAINT [FK_Estudio_Hospital] FOREIGN KEY ([SK_Hospital]) REFERENCES [dbo].[DimHospital] ([SK_Hospital]),
-	CONSTRAINT [FK_Estudio_Medico] FOREIGN KEY ([SK_Medico]) REFERENCES [dbo].[DimMedico] ([SK_Medico])
+	CONSTRAINT [FK_Estudio_Medico] FOREIGN KEY ([SK_Medico]) REFERENCES [dbo].[DimMedico] ([SK_Medico]),
+	CONSTRAINT [FK_Estudio_Fecha] FOREIGN KEY ([ID_Fecha]) REFERENCES [dbo].[DimTiempo] ([ID_Tiempo]),
+	CONSTRAINT [FK_Estudio_FeEst] FOREIGN KEY ([ID_Fecha_Estudio]) REFERENCES [dbo].[DimTiempo] ([ID_Tiempo]),
+    CONSTRAINT [FK_Estudio_FeEnt] FOREIGN KEY ([ID_Fecha_Entrega]) REFERENCES [dbo].[DimTiempo] ([ID_Tiempo])
 );
